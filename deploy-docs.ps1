8570b1ed90af74f16caeffd88ac5bb99a09bf3b4
Julian Knight <jk.github@totallyinformation.com>
2020-01-25 22:27:05 +0000

    Better cmds than those given by vuepress

--- deploy-docs.ps1
+++ deploy-docs.ps1
@@ -15,6 +15,10 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:TotallyInformation/node-red-contrib-infocache.git master:gh-pages
+#git push -f git@github.com:TotallyInformation/node-red-contrib-infocache.git master:gh-pages
 
-cd -
+git remote add origin https://github.com/TotallyInformation/node-red-contrib-infocache.git
+git branch --set-upstream-to origin/gh-pages
+git push -f -u origin master:gh-pages
+
+cd ../..
